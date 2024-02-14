ba119c487a352a5d782d8ce675ac4e9360c8f0e7
Zwiieb <100715760+Zwiieb@users.noreply.github.com>
2024-02-14 15:44:30 +0100

    ajout test integration

--- test-integration/commerce-app/panier.py
+++ test-integration/commerce-app/panier.py
@@ -3,15 +3,18 @@ from produit import *
 class Panier:
     total = 0
     produits = []
-    def __init__(self, produits):
-            self.produits = produits
+
+    def getProduits(self):
+        return self.produits
+    def getTotal(self):
+        return self.total
 
     def add(self, produit, nb):
         if nb > 0:
             self.total += produit.getPrix() * nb
             produit.setStock(produit.getStock() - nb)
             self.produits.append(produit)
-            
+
     def remove(self, produit, nb):
         if nb > 0:
             self.total -= produit.getPrix() * nb
--- test-integration/commerce-app/produit.py
+++ test-integration/commerce-app/produit.py
@@ -1,14 +1,17 @@
 class Produit:
-        def __init__(self, nb, prix, remise):
-            self.stock = nb
-            self.prix = prix
-            self.remise = remise
+    def __init__(self,nom, nb, prix, remise):
+        self.nom = nom
+        self.stock = nb
+        self.prix = prix
+        self.remise = remise
 
-        def getPrix(self):
-              return self.prix
-        def getStock(self):
-              return self.stock
-        def getRemise(self):
-              return self.remise
-        def setStock(self,nb):
-              self.stock = nb
+    def getNom(self):
+        return self.nom
+    def getPrix(self):
+          return self.prix
+    def getStock(self):
+          return self.stock
+    def getRemise(self):
+          return self.remise
+    def setStock(self,nb):
+          self.stock = nb
