28f90975495625b3d54ae9b6847b2cdc33c0e354
YounessM78 <ymachdoud@gmail.com>
2024-02-26 17:32:07 +0100

    selenium

--- e2e-tests/test_python_website.py
+++ e2e-tests/test_python_website.py
@@ -5,11 +5,19 @@ import time
 
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
-time.sleep(10)
-search_box = driver.find_element(By.NAME, "q")
+time.sleep(2)
 
-search_box.send_keys("python")
-search_box.send_keys(Keys.RETURN)
-time.sleep(5)
-assert "No results found." not in driver.page_source
+events_link = driver.find_element(By.XPATH, '//*[@id="events"]/a')
+events_link.click()
+
+time.sleep(2)
+
+python_events_link = driver.find_element(By.XPATH, '//*[@id="events"]/ul/li[1]/a')
+python_events_link.click()
+
+driver.get("https://www.python.org/events/")
+
+python_events_link2 = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div/div/ul/li[2]/h3/a')
+python_events_link2.click()
+
 driver.quit()
