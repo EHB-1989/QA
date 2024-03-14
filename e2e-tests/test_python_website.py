1b90fe47df640acab2cdd0b1613767cc212489ca
Zwiieb <100715760+Zwiieb@users.noreply.github.com>
2024-03-14 15:24:05 +0100

    python download test

--- e2e-tests/test_python_website.py
+++ e2e-tests/test_python_website.py
@@ -3,37 +3,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-# driver = webdriver.Firefox()
-# driver.get("http://www.python.org")
-# time.sleep(10)
-# search_box = driver.find_element(By.XPATH, '//input[@id="id-search-field"]')
-# search_box.send_keys("python")
-# search_box.send_keys(Keys.RETURN)
-# time.sleep(5)
-# assert "No results found." not in driver.page_source
-# driver.quit()
-
 
 # scenario check python event
 
-driver = webdriver.Firefox()
+driver = webdriver.Edge()
 driver.get("http://www.python.org")
 
-# cliquer sur le button event 
-button_locator = driver.find_element(By.XPATH, '//*[@id="events"]/a')
+# cliquer sur le button download 
+button_locator = driver.find_element(By.XPATH, '//*[@id="downloads"]/a')
 button_locator.click()
 
 # Verifier qu'on est bien sur la page des events 
 
-
-# cliquer sur l'evenement PyCon Namibia 2024
-
-event_locator = driver.find_element()
-event_locator.click()
-
-
-# Verifier qu'on est bien sur la page de l'evenemt 
-
+# cliquer sur le button download 
+driver.get(driver.current_url)
+events_link = driver.find_element(By.XPATH, '//*[@id="touchnav-wrapper"]/header/div/div[2]/div/div[2]/p/a')
+x = str(events_link.text)
+assert str("Download Python 3.12.2") in x
+print('ok')
 
 driver.quit()
 
