05757a5654deda6d6b2d886beef8f466e9e9ab08
Zwiieb <100715760+Zwiieb@users.noreply.github.com>
2024-04-04 15:08:54 +0200

    exercice 1

---
+++ examen/unit-tests/tests.py
@@ -0,0 +1,44 @@
+import unittest
+from app import Livre,Bibliotheque
+
+
+class TestLivre(unittest.TestCase):
+    def setUp(self):
+        self.livre = Livre("1984", "George Orwell")
+
+    def test_emprunter(self):
+        self.assertTrue(self.livre.emprunter())
+        self.assertTrue(self.livre.est_emprunte)
+
+    def test_retourner(self):
+        self.livre.emprunter()
+        self.assertTrue(self.livre.retourner())
+        self.assertFalse(self.livre.est_emprunte)
+
+class TestBibliotheque(unittest.TestCase):
+    bibliotheque = Bibliotheque()
+    livre1 = Livre("Naruto", "Masashi Kishimoto")
+    livre2 = Livre("One Piece", "Eiichirō Oda")
+    bibliotheque.ajouter_livre(livre1)
+    bibliotheque.ajouter_livre(livre2)
+
+    def test_ajouter_livre(self):
+        self.assertEqual(len(self.bibliotheque.livres), 2)
+
+    def test_emprunter_livre(self):
+        self.assertTrue(self.bibliotheque.emprunter_livre("Naruto"))
+        self.assertTrue(self.livre1.est_emprunte)
+
+    def test_retourner_livre(self):
+        self.bibliotheque.emprunter_livre("Naruto")
+        self.assertTrue(self.bibliotheque.retourner_livre("Naruto"))
+        self.assertFalse(self.livre1.est_emprunte)
+
+    def test_emprunter_livre_inexistant(self):
+        self.assertFalse(self.bibliotheque.emprunter_livre("Bleach"))
+
+    def test_retourner_livre_non_emprunte(self):
+        self.assertFalse(self.bibliotheque.retourner_livre("Bleach"))
+
+if __name__ == '__main__':
+    unittest.main()
