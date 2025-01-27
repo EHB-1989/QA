f721a9169183bc5a1e6d15df7a6010233a8cc6cb
Rahzbob <tom.dejardin94@gmail.com>
2025-01-27 15:59:05 +0100

    Test panier

--- integration-tests/commerce-app/panier.py
+++ integration-tests/commerce-app/panier.py
@@ -1,3 +1,7 @@
+import unittest
+from unittest.mock import MagicMock
+from produit import Produit
+
 # class Panier:
 #     def __init__(self):
 #         self.produits = []
@@ -29,3 +33,15 @@ class Panier:
         for produit, quantite in self.produits.items():
             total += produit.calculer_prix_apres_remise() * quantite
         return total
+
+class TestPanier(unittest.TestCase):
+    def test_ajouter_produit(self):
+        panier_test = Panier()
+        produit_test = Produit("Livre", 10, 50)
+        panier_test.ajouter_produit(produit_test, 2)
+        self.assertEqual(panier_test.produits[produit_test], 2)
+        total = panier_test.calculer_total()
+        self.assertEqual(total, 20)
+
+if __name__ == "__main__":
+    unittest.main()
