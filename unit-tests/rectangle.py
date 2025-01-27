0ef9ebb32bc3aa3ba89a2d07b72e81ffb04bf41f
Rahzbob <tom.dejardin94@gmail.com>
2025-01-27 14:23:48 +0100

    Test rectangle

--- unit-tests/rectangle.py
+++ unit-tests/rectangle.py
@@ -1,4 +1,5 @@
-# rectangle.py
+import unittest
+
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
@@ -6,3 +7,13 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
+
+class TestRectangle(unittest.TestCase):
+    def test_area(self):
+        r = Rectangle(2, 3)
+        self.assertEqual(r.area(), 6)
+        r = Rectangle(5, 5)
+        self.assertEqual(r.area(), 25)
+
+if __name__ == "__main__":
+    unittest.main()
