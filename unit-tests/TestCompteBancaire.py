3d9e0e75e785bf13c84ef37ec8fbb3b7f0a3cfbc
JBenrejdal <jarod.benrejdal@gmail.com>
2025-01-27 14:46:00 +0100

    add TU Comptebancaire

--- unit-tests/TestCompteBancaire.py
+++ unit-tests/TestCompteBancaire.py
@@ -1,22 +1,5 @@
 import unittest
-class CompteBancaire:
-    def __init__(self, solde_initial=0):
-        self.solde = solde_initial
-
-    def depot(self, montant):
-        if montant > 0:
-            self.solde += montant
-            return True
-        return False
-
-    def retrait(self, montant):
-        if montant > 0 and self.solde - montant >= 0:
-            self.solde -= montant
-            return True
-        return False
-
-    def get_solde(self):
-        return self.solde
+from CompteBancaire import CompteBancaire
 
 class TestCompteBancaire(unittest.TestCase):
     
@@ -24,28 +7,28 @@ class TestCompteBancaire(unittest.TestCase):
         compte = CompteBancaire()
         self.assertEqual(compte.get_solde(), 0)
 
-    def test_initialisation_avec_solde_personnalise(self):
+    def test_initialisation_avec_solde_perso(self):
         compte = CompteBancaire(100)
         self.assertEqual(compte.get_solde(), 100)
-
+    #Dépôt ok
     def test_depot_valide(self):
         compte = CompteBancaire(50)
         resultat = compte.depot(50)
         self.assertTrue(resultat)
         self.assertEqual(compte.get_solde(), 100)
-
+    #Dépôt ko
     def test_depot_invalide(self):
         compte = CompteBancaire(50)
         resultat = compte.depot(-10)
         self.assertFalse(resultat)
         self.assertEqual(compte.get_solde(), 50)
-
+    #Retrait ok
     def test_retrait_valide(self):
         compte = CompteBancaire(100)
         resultat = compte.retrait(50)
         self.assertTrue(resultat)
         self.assertEqual(compte.get_solde(), 50)
-
+    #Retrait ko
     def test_retrait_invalide_montant_negatif(self):
         compte = CompteBancaire(100)
         resultat = compte.retrait(-10)
