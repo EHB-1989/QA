d36a3bd71691c593c5b62d508e5f0b1c50dd82e0
Mussard <mussardwassim92@gmail.com>
2025-01-27 17:03:37 +0100

    exo blog app api done

--- integration_tests/blog-app_api/poster.py
+++
@@ -1,4 +0,0 @@
-class Poster:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
---
+++ integration_tests/blog_app_api/poster.py
@@ -0,0 +1,6 @@
+#poster.py
+
+class Poster:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
