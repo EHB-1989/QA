cc698654cdf5011240633a4e9b07951b4267ac22
Rahzbob <tom.dejardin94@gmail.com>
2025-01-27 14:32:48 +0100

    Test CompteBancaire

--- unit-tests/CompteBancaire.py
+++ unit-tests/CompteBancaire.py
@@ -1,3 +1,5 @@
+import unittest
+
 class CompteBancaire:
     def __init__(self, solde_initial=0):
         self.solde = solde_initial
@@ -16,3 +18,27 @@ class CompteBancaire:
 
     def get_solde(self):
         return self.solde
+
+class TestCompteBancaire(unittest.TestCase):
+    def test_get_solde(self):
+        c = CompteBancaire(100)
+        self.assertEqual(c.get_solde(), 100)
+
+    def test_depot(self):
+        c = CompteBancaire()
+        self.assertTrue(c.depot(100))
+        self.assertEqual(c.get_solde(), 100)
+        self.assertFalse(c.depot(-100))
+        self.assertEqual(c.get_solde(), 100)
+
+    def test_retrait(self):
+        c = CompteBancaire(100)
+        self.assertTrue(c.retrait(50))
+        self.assertEqual(c.get_solde(), 50)
+        self.assertFalse(c.retrait(-50))
+        self.assertEqual(c.get_solde(), 50)
+        self.assertFalse(c.retrait(100))
+        self.assertEqual(c.get_solde(), 50)
+
+if __name__ == "__main__":
+    unittest.main()
