f77c794352c82b21c30e4a2703a3ee4428fb1068
Baptiste FOURMONT <baptiste_fourmont@tutanota.com>
2025-02-13 12:06:16 +0100

    [Add] New tests

--- e2e-tests/blog-app/app.py
+++ e2e-tests/blog-app/app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 posts = []
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template_string(
-        """
+    return render_template_string("""
         <h1>Blog</h1>
         <a href="/create">Créer un Article</a>
         {% for post in posts %}
@@ -17,29 +15,23 @@ def index():
                 <p>{{ post.content }}</p>
             </div>
         {% endfor %}
-    """,
-        posts=posts,
-    )
+    """, posts=posts)
 
-
-@app.route("/create", methods=["GET", "POST"])
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    if request.method == "POST":
-        title = request.form["title"]
-        content = request.form["content"]
-        posts.append({"title": title, "content": content})
-        return redirect(url_for("index"))
-    return render_template_string(
-        """
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        posts.append({'title': title, 'content': content})
+        return redirect(url_for('index'))
+    return render_template_string("""
         <form method="post">
             Titre: <input type="text" name="title"><br>
             Contenu:<br>
             <textarea name="content"></textarea><br>
             <input type="submit" value="Créer">
         </form>
-    """
-    )
+    """)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
