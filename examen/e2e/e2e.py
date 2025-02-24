3f07fe7b590e2438900e94f20cf4eaa87e1286d3
Ethazz-afk <omar.hammami@etu.u-pec.fr>
2025-02-24 16:14:19 +0100

    Ajout des test unitaires pour e2e

--- examen/e2e/e2e.py
+++ examen/e2e/e2e.py
@@ -4,25 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-try : 
-        # Initialisation du navigateur Chrome car le Driver Firefox ne passe pas sur mon Linux
-        driver = webdriver.Chrome()  
+class PythonOrgTests(unittest.TestCase):
 
-        #Naviguer à la page d'accueil
-        driver.get("https://www.python.org")
-        time.sleep(1) 
+    @classmethod
+    def setUpClass(cls):
+        # Initialisation du navigateur (Chrome ici, sinon changer pour Firefox)
+        cls.driver = webdriver.Chrome()  
+        cls.driver.maximize_window()  
 
+    def test_acceder_beginner_documentation(self):
         # Accéder à la section Documentation
-        doc_link = driver.find_element(By.XPATH, '//li[@id="documentation"]/a')
+        self.driver.get("https://www.python.org")
+        time.sleep(1)
+        doc_link = self.driver.find_element(By.XPATH, '//li[@id="documentation"]/a')
         doc_link.click()
-        time.sleep(1)  # Attendre le chargement
-
-        event_link = driver.find_element(By.LINK_TEXT, "Beginner’s Guide")
+        time.sleep(1) 
+        event_link = self.driver.find_element(By.LINK_TEXT, "Beginner’s Guide")
         event_link.click()
 
-        # Vérifier qu'on est bien sur la page de la documentation beginner
-        assert "BeginnersGuide - Python Wiki" in driver.title
-        
-finally:
+        # Vérifier qu'on est bien sur la page de la documentation
+        self.assertIn("BeginnersGuide - Python Wiki" , self.driver.title)
 
-    driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
