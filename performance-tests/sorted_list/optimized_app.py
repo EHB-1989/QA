bc5a8de1f1eeb8629363a6d78f43eaa7133c23c6
Tomy Da Rocha <91685743+tomy-da-rocha@users.noreply.github.com>
2025-02-24 11:47:13 +0100

    add perfomance tests
    - tried some tweaks to optimize server, still crashing

--- performance-tests/sorted_list/optimized_app.py
+++ performance-tests/sorted_list/optimized_app.py
@@ -1,15 +1,15 @@
+from Scripts.pywin32_testall import this_dir
 from flask import Flask, request, jsonify
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/sort', methods=['POST'])
 def sort_numbers():
     data = request.get_json().get('numbers', [])
-    sorted_list = []
-    for number in data:
-        sorted_list.append(number)
-        sorted_list = sorted(sorted_list)
+    sorted_list = sorted(data)
     return jsonify(sorted_list)
 
-if __name__ == '__main__':
-    app.run(debug=False, threaded=True, process=16)
+
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=5000, threads=32)
