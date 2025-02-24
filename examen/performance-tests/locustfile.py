b6023918a8121b9a9915ab68ba6f186eb3fb2b80
Ethazz-afk <omar.hammami@etu.u-pec.fr>
2025-02-24 16:25:39 +0100

    Ajout du locusfile

---
+++ examen/performance-tests/locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, task, between
+
+class UserBehavior(HttpUser):
+    wait_time = between(1, 2) 
+
+    @task
+    def get_user(self):
+        user_id = 1  
+        self.client.get(f"/user/{user_id}")
+
+
